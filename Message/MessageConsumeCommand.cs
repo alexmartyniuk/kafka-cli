610433f95a2bc59f5a0a064ff41e5573c0c43295
Aleksander Martyniuk <aleksander.martyniuk>
2019-12-02 10:09:30 +0100

    Removed forgetme parameter

--- Message/MessageConsumeCommand.cs
+++ Message/MessageConsumeCommand.cs
@@ -21,9 +21,6 @@ namespace Scania.Kafka.Tool.Cli.Message
         [Option("-g|--group", Description = "Group ID")]
         public string GroupId { get; } = "kafka-cli";
 
-        [Option("-fm|--forgetme", Description = "Group ID")]
-        public bool ForgetMe { get; } = false;
-
         [Option("-c|--commit", Description = "Commit the message. This prevents from receiving the same message twice for the one Group ID.")]
         public bool Commit { get; } = false;
 
@@ -34,13 +31,9 @@ namespace Scania.Kafka.Tool.Cli.Message
         {
             var config = ConfigService.Get();
 
-            var consumerGroupId = ForgetMe ?
-                GroupId + "-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") :
-                GroupId;
-
             console.WriteLine($"Waiting for messages in {TopicName}:");
 
-            KafkaClient.ConsumeMessage(TopicName, consumerGroupId, Commit, (string topicInfo, string message) =>
+            KafkaClient.ConsumeMessage(TopicName, GroupId, Commit, (string topicInfo, string message) =>
             {
                 console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss.ff")}] at {topicInfo}: {message}");
                 if (Pause > 0)
